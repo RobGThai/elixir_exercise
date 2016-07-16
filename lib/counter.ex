87e7b88c8a09db5c7ab89a3edd266ba4f3463027
RobGThai <thisisrobg@gmail.com>
2016-07-16 14:50:24 +0700

    Implement increase and decrease function.

--- lib/counter.ex
+++ lib/counter.ex
@@ -1,2 +1,21 @@
 defmodule Counter do
+
+  def start do
+    Agent.start_link(fn -> 0 end, name: __MODULE__)
+  end
+
+  def get(counter) do
+    Agent.get(counter, fn n -> n end)
+  end
+
+  def increase(counter) do
+    Agent.update(counter, fn n -> n + 1 end)
+    Counter.get(counter)
+  end
+
+  def decrease(counter) do
+    Agent.update(counter, fn n -> n - 1 end)
+    Counter.get(counter)
+  end
+
 end
--- test/counter_test.exs
+++ test/counter_test.exs
@@ -5,4 +5,27 @@ defmodule CounterTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "Counter is working" do
+    {_, counter} = Counter.start()
+    assert Process.alive?(counter)
+  end
+
+  test "Get value 0" do
+    {_, counter} = Counter.start()
+    assert Process.alive?(counter)
+    assert Counter.get(counter) == 0
+  end
+
+  test "Increase" do
+    {_, counter} = Counter.start()
+    assert Process.alive?(counter)
+    assert Counter.increase(counter) == 1
+  end
+
+  test "Decrease" do
+    {_, counter} = Counter.start()
+    assert Process.alive?(counter)
+    assert Counter.decrease(counter) == -1
+  end
 end
